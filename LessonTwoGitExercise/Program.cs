8549bc69ec09b3fc7b380924f7868c31547562f5
ccartagena879 <ccartagena879@gmail.com>
2019-02-03 20:31:26 -0600

    added tell me your name

---
+++ LessonTwoGitExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LessonTwoGitExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Tell me your name");
+        }
+    }
+}
